151834a8c9029cffe391269f5c7e71cb6b55c596
Rafiu Ayomide <rafiuayomide1822@gmail.com>
2022-12-09 14:47:41 -0800

    alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - main block
  * Return : Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
