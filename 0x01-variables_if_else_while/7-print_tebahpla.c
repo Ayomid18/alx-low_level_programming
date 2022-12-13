b5d3f3837d7f4aa76f6e2dfe8a11fb0fcfc87ff8
Rafiu Ayomide <rafiuayomide1822@gmail.com>
2022-12-13 01:37:32 -0800

    variable if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Return 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; i--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
